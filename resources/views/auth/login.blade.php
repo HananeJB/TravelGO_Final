0e3928992240e2afc9d2446c761a7ce71737c9ed
HananeJB <jabouhanane@gmail.com>
2021-09-12 18:39:24 +0200

    home page added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,8 +16,9 @@
                                         <h4 class="text-dark mb-4">{{ __('Login') }}</h4>
                                     </div>
                                     <form class="user" method="POST" action="{{ route('login') }}">
+                                        @csrf
                                         <div class="form-group">
-                                            <input class="form-control form-control-user" type="email" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." name="email" value="{{ old('email') }}" required autocomplete="email" autofocus >
+                                            <input class="form-control form-control-user" type="email" id=email" aria-describedby="emailHelp" placeholder="Enter Email Address..." name="email" value="{{ old('email') }}" required autocomplete="email" autofocus >
                                         </div>
                                         @error('email')
                                         <span class="invalid-feedback" role="alert">
@@ -35,17 +36,19 @@
                                         @enderror
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
-                                                <div class="form-check"
-                                                ><input class="form-check-input custom-control-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                                <div class="form-check">
+                                                    <input class="form-check-input custom-control-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                                                     <label class="form-check-label custom-control-label" for="formCheck-1"> {{ __('Remember Me') }}</label>
                                                 </div>
                                             </div>
-                                        </div><button class="btn btn-primary btn-block text-white btn-user" type="submit" style="background: #f85959;border-color: rgb(255, 255, 255);border-top-color: rgb(255,;border-right-color: 255,;border-bottom-color: 255);border-left-color: 255,;"> {{ __('Login') }}</button>
+                                        </div>
+                                        <button class="btn btn-primary btn-block text-white btn-user" type="submit" style="background: #f85959;border-color: rgb(255, 255, 255);border-top-color: rgb(255,;border-right-color: 255,;border-bottom-color: 255);border-left-color: 255,;"> {{ __('Login') }}</button>
                                         <hr>
                                         <hr>
-                                    </form>
+
                                     <div class="text-center"><a class="small" href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a></div>
                                     <div class="text-center"><a class="small" href="{{ route('register') }}">Create an Account!</a></div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
